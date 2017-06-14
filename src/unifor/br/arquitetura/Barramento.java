8e3414c2a08c6f4bc2b3fb46d166e3bd02ead53c
Mario Hercules Portela <mario@Marios-MacBook-Pro.local>
2017-06-14 20:22:22 -0300

    Add Barramento.java

---
+++ src/unifor/br/arquitetura/Barramento.java
@@ -0,0 +1,20 @@
+package unifor.br.arquitetura;
+
+public class Barramento {
+	
+	RAM ram;
+	ModuloES moduloES;
+	CPU cpu;
+	
+		
+	public Barramento(ModuloES moduloES) {
+		this.moduloES = moduloES;
+		this.ram = new RAM(this);
+		this.cpu = new CPU(this);
+	}
+
+	public void addInstrucao(byte[] instrucao){
+		ram.addInstrucao(instrucao);
+	}
+	
+}
