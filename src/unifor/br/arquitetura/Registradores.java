e43221ea664f8257fca6e77fc223ae1aa33f2476
Mario Hercules Portela <mario@Marios-MacBook-Pro.local>
2017-06-14 20:23:51 -0300

    Add Registradores.java

---
+++ src/unifor/br/arquitetura/Registradores.java
@@ -0,0 +1,48 @@
+package unifor.br.arquitetura;
+
+public class Registradores {
+	
+	String a = "0";
+	String b = "0";
+	String c = "0";
+	String d = "0";
+	int ci = 1;
+	
+	public Registradores() {
+		super();
+	}
+	
+	public void setReg(String reg, String val){
+		
+		if(reg.charAt(0) == 'A'){
+			a = val;
+		}else if(reg.charAt(0) == 'B'){
+			b = val;
+		}else if(reg.charAt(0) == 'C'){
+			c = val;
+		}else if(reg.charAt(0) == 'D'){
+			d = val;
+		}else{
+			System.err.println("Registrador invalido");;
+		}
+		
+	}
+	
+	public String getReg(String reg){
+		
+		if(reg.charAt(0) == 'A'){
+			return a;
+		}else if(reg.charAt(0) == 'B'){
+			return b;
+		}else if(reg.charAt(0) == 'C'){
+			return c;
+		}else if(reg.charAt(0) == 'D'){
+			return d;
+		}else{
+			System.err.println("Registrador invalido");;
+			return null;			
+		}
+		
+	}
+	
+}
